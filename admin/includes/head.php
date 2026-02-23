987682f8b1dc27add897e66d9dfc2150684adca3
VinlaC <calvinsetiawan166@gmail.com>
2026-02-23 22:44:11 +0700

    Admin panel

---
+++ admin/includes/head.php
@@ -0,0 +1,46 @@
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--== Favicon ==-->
+    <link rel="icon" href="../assets/images/favicon/icon-fivit.png">
+	<!--plugins-->
+	<link href="assets/plugins/simplebar/css/simplebar.css?v=20260223" rel="stylesheet" />
+	<link href="assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css?v=20260223" rel="stylesheet" />
+	<link href="assets/plugins/metismenu/css/metisMenu.min.css?v=20260223" rel="stylesheet" />
+	<!-- loader-->
+	<link href="assets/css/pace.min.css?v=20260223" rel="stylesheet" />
+	<script src="assets/js/pace.min.js"></script>
+	<!-- Bootstrap CSS -->
+	<link href="assets/css/bootstrap.min.css?v=20260223" rel="stylesheet">
+	<link href="assets/css/bootstrap-extended.css?v=20260223" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+	<link href="assets/css/app.css?v=20260223" rel="stylesheet">
+	<link href="assets/css/icons.css?v=20260223" rel="stylesheet">
+	<!-- Theme Style CSS -->
+	<link rel="stylesheet" href="assets/css/dark-theme.css?v=20260223" />
+	<link rel="stylesheet" href="assets/css/semi-dark.css?v=20260223" />
+	<link rel="stylesheet" href="assets/css/header-color.css?v=20260223" />
+	<style>
+		:root {
+			--bs-primary: #32C7D8;
+			--bs-primary-rgb: 50, 199, 216;
+			--bs-link-color: #32C7D8;
+			--bs-link-hover-color: #2AAAB9;
+		}
+		.btn-primary {
+			background-color: #32C7D8;
+			border-color: #32C7D8;
+		}
+		.btn-primary:hover,
+		.btn-primary:focus,
+		.btn-primary:active {
+			background-color: #2AAAB9;
+			border-color: #2AAAB9;
+		}
+		.text-purple {
+			color: #32C7D8 !important;
+		}
+	</style>
+	<title>Fivit - Admin</title>
+</head>
