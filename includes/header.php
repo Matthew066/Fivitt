2021365ced4d3383d61c9cf497d6d184360a63ed
Matthew066 <matthewabednego63@gmail.com>
2026-03-01 00:48:42 +0700

    benerin login header

--- includes/header.php
+++ includes/header.php
@@ -11,6 +11,7 @@ if (is_string($extraStyles)) {
 <head>
     <meta charset="UTF-8">
     <title><?= $pageTitle ?? 'Fivit' ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Fivit - Login</title>
