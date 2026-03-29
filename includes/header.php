b20f2bce44e1760ab1324a8d776e0d1b960f6152
phyliciatiffany456-ai <phyliciatiffany456@gmail.com>
2026-03-29 19:06:21 +0700

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -55,13 +55,14 @@ $isLoggedIn = isset($_SESSION['user_id']);
         <a class="drawer-link sub" href="gym.php">Gym booking</a>
         <a class="drawer-link sub" href="coach.php">Coach Directory</a>
         <a class="drawer-link sub" href="coach_sessions.php">Coach Sessions</a>
-        <a class="drawer-link sub" href="community.php">Community Hub</a>
 
         <div class="drawer-section">Canteen</div>
         <a class="drawer-link sub<?= $isLoggedIn ? '' : ' is-disabled' ?>" href="<?= $isLoggedIn ? 'foodselection.php' : 'login.php' ?>">Food Selection</a>
 
         <div class="drawer-section">Event</div>
         <a class="drawer-link sub<?= $isLoggedIn ? '' : ' is-disabled' ?>" href="<?= $isLoggedIn ? 'education.php' : 'login.php' ?>">Education</a>
+        <div class="drawer-section">Community</div>
+        <a class="drawer-link sub<?= $isLoggedIn ? '' : ' is-disabled' ?>" href="<?= $isLoggedIn ? 'community.php' : 'login.php'?>"> Community Hub</a>
     </nav>
 </aside>
 
