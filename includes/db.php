36bc3a459b5861bfa0410e40c180c9fc3da988ee
phyliciatiffany456-ai <phyliciatiffany456@gmail.com>
2026-04-23 21:09:18 +0700

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,16 +1,16 @@
 <?php
 $host = 'localhost';
-$db   = 'fivit_db';
-$user = 'root';
-$pass = '';
+$db   = 'inoh4572_fivit_db';
+$user = 'USERNAME_DB_KAMPUS';
+$pass = 'PASSWORD_DB_KAMPUS';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
 $options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
+    PDO::ATTR_EMULATE_PREPARES => false,
 ];
 
 try {
